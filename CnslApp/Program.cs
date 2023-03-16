04f1eb2d645e5441dff38283333c765c9ff45bc1
eviolspirid777 <eviolspirid@gmail.com>
2023-03-16 10:46:32 +0300

    qqq

--- CnslApp/Program.cs
+++ CnslApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.Json;
 using PrsnLib;
 using FileFunc;
-using TableFunc;
+using CnslApp;
 //string Path = @"content.json";
 
 internal class Program
@@ -18,7 +18,7 @@ internal class Program
     }
 
 
-    static void EditNode(int count, ref List<Person> people)
+    static void EditNode(int count, List<Person> persons)
     {
         Console.WriteLine("\nВы хотите изменить запись?(y/n)");
         var sw = Console.ReadLine();
@@ -26,7 +26,7 @@ internal class Program
         {
             Console.WriteLine("Введите номер записи, которую хотите изменить:");
             string? num = Console.ReadLine();
-            if (int.TryParse(num, out int number) && number <= people.Count)
+            if (int.TryParse(num, out int number) && number <= persons.Count)
             {
                 number--;
                 Console.WriteLine("Что вы хотите изменить:\n1)ФИО\n2)Город\n3)Почтовый индекс\n4)Улицу\n5)Почту\n6)Телефон\n7)Факультет\n8)Курс\n9)Группу\n10)Специальность\n");
@@ -34,41 +34,41 @@ internal class Program
                 switch (t)
                 {
                     case "1":
-                        people[number].Fio.Surname = EnterResult("Фамилия");
-                        people[number].Fio.Name = EnterResult("Имя");
-                        people[number].Fio.Patron = EnterResult("Отчество");
+                        persons[number].Fio.Surname = EnterResult("Фамилия");
+                        persons[number].Fio.Name = EnterResult("Имя");
+                        persons[number].Fio.Patron = EnterResult("Отчество");
                         break;
                     case "2":
-                        people[number].Address.City = EnterResult("Город");
+                        persons[number].Address.City = EnterResult("Город");
                         break;
                     case "3":
-                        people[number].Address.PstIndex = EnterResult("Почтовый индекс");
+                        persons[number].Address.PstIndex = EnterResult("Почтовый индекс");
                         break;
                     case "4":
-                        people[number].Address.Street = EnterResult("Улица");
+                        persons[number].Address.Street = EnterResult("Улица");
                         break;
                     case "5":
-                        people[number].Contacts.Mail = EnterResult("Почта");
+                        persons[number].Contacts.Mail = EnterResult("Почта");
                         break;
                     case "6":
-                        people[number].Contacts.Phone = EnterResult("Телефон");
+                        persons[number].Contacts.Phone = EnterResult("Телефон");
                         break;
                     case "7":
-                        people[number].Curriculum.Faculty = EnterResult("Факультет");
+                        persons[number].Curriculum.Faculty = EnterResult("Факультет");
                         break;
                     case "8":
-                        people[number].Curriculum.Course = EnterResult("Курс");
+                        persons[number].Curriculum.Course = EnterResult("Курс");
                         break;
                     case "9":
-                        people[number].Curriculum.Group = EnterResult("Группа");
+                        persons[number].Curriculum.Group = EnterResult("Группа");
                         break;
                     case "10":
-                        people[number].Curriculum.Specialty = EnterResult("Специальность");
+                        persons[number].Curriculum.Specialty = EnterResult("Специальность");
                         break;
                     default:
                         Console.WriteLine("Введен неправильный символ!");
                         Console.ReadKey();
-                        EditNode(count, ref people);
+                        EditNode(count, persons);
                         break;
                 }
             }
@@ -83,10 +83,10 @@ internal class Program
         {
             Console.WriteLine("Вы ввели неправильный символ!");
             Thread.Sleep(1000);
-            EditNode(count, ref people);
+            EditNode(count, persons);
         }
     }
-    static void FillExmp(ref Person temper)
+    static void FillExmp(Person temper)
     {
         Console.WriteLine("Введите ФИО:\n Имя");
         temper.Fio.Name = Console.ReadLine();
@@ -150,7 +150,7 @@ internal class Program
             var exp = JsonSerializer.Deserialize<List<Person>>(jsonString)!; //ПРЕДСТАВИТЬ ВВИДЕ МАССИВА И ОТРАБОТАТЬ КАЖДЫЙ ЭЛЕМЕНТ ЧЕРЕЗ ЦИКЛ
             ExmpMenu(exp, out int counter);
             Console.WriteLine();
-            EditNode(counter, ref exp);
+            EditNode(counter, exp);
             jsonString = JsonSerializer.Serialize(exp, FileWork.Options());
             FileWork.WriteText(FileWork.GetPath(), jsonString);
             Console.Clear();
@@ -177,7 +177,7 @@ internal class Program
                 persons = JsonSerializer.Deserialize<List<Person>>(File.ReadAllText(FileWork.GetPath()));
             }
             Person temp = new();
-            FillExmp(ref temp);
+            FillExmp(temp);
             Console.Clear();
             persons.Add(temp);
             string jsonString = JsonSerializer.Serialize(persons, FileWork.Options());
